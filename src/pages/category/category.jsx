5a4cd6052dcf09ec44484c738718a7d4795c28d6
Modail <919471638@qq.com>
2021-01-22 20:15:24 +0800

    react-admin Test

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import { Card, Table, Button } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { dataSource, columns } from "../../config/tableconfig";
 
 export default class Category extends Component {
   render() {
-    return <div>商品分类</div>;
+    const card = {
+      title: "商品管理",
+      extra: <Button type="primary" shape="circle" icon={<SearchOutlined />} />,
+    };
+    return (
+      <Card title={card.title} extra={card.extra} style={{ width: "100%" }}>
+        <Table dataSource={dataSource} columns={columns} />
+      </Card>
+    );
   }
 }
